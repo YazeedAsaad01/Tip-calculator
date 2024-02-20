459bc24e0b5a5e7cd8d4ff6ae5f9a1dd44fa6c3e
Yazeed Asaad <159422722+YazeedAsaad01@users.noreply.github.com>
2024-02-19 21:27:30 -0800

    Update code.js
    added other Tip

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ const input = document.querySelector('#amount');
 const button5 = document.querySelector('#button1');
 const button10 = document.querySelector('#button2');
 const button15 = document.querySelector('#button3');
+const buttonOther = document.querySelector('#button4');
+const inputOther = document.querySelector('#other');
 
 function value(amount) {
     const value = input.value;
@@ -9,12 +11,17 @@ function value(amount) {
     input.value = '';
 }
 
-button5.addEventListener('click', () => {
-    value(5);
-});
-button10.addEventListener('click', () => {
-    value(10);
-});
-button15.addEventListener('click', () => {
-    value(15);
+button5.addEventListener('click', () => {value(5)});
+button10.addEventListener('click', () => {value(10)});
+button15.addEventListener('click', () => {value(15)});
+
+buttonOther.addEventListener('click', () => {
+    const inputValue = parseFloat(inputOther.value);
+    if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 100) {
+        value(inputValue);
+        inputOther.value = '';
+    } else {
+        alert('Please enter a valid number between 0 and 100.');
+        inputOther.value = '';
+    }
 });
